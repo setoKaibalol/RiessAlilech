558b31b4470c0d468462ecefd6e060c5259e2aeb
setoKaibalol <kaibacorpbsc@gmail.com>
2023-03-22 16:33:38 +0100

    alot

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,9 +1,18 @@
 import NextAuth from "next-auth"
 import { Session, User } from "next-auth"
 
+import NextAuth from "next-auth"
+
 declare module "next-auth" {
-	interface Session {}
-	interface User {
-		role: string | null
+	/**
+	 * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+	 */
+	interface Session {
+		user: {
+			/** The user's postal address. */
+			image: string | null | undefined
+			role: string | null | undefined
+			id: string | null | undefined
+		}
 	}
 }
