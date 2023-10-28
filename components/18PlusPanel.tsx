421bd583237b6d47f18dbd92a171fcb6a07a4ac9
setoKaibalol <kaibacorpbsc@gmail.com>
2023-10-28 22:09:37 +0200

    update 18pluspanel

--- components/18PlusPanel.tsx
+++ components/18PlusPanel.tsx
@@ -1,37 +1,83 @@
 import Link from "next/link"
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 type Props = {}
 
 function EightheenPlusPanel({}: Props) {
-	const [open, setOpen] = useState(true)
+	const [open, setOpen] = useState(false)
+
+	useEffect(() => {
+		const ageVerified = localStorage.getItem("ageVerified")
+		if (ageVerified) {
+			setOpen(false)
+		} else {
+			setOpen(true)
+		}
+	}, [])
 
 	const LeaveSiteButton = () => {
 		const handleLeave = () => {
-			window.history.back()
+			window.location.href = "https://www.google.com"
 		}
 
 		return (
-			<button className=" text-black font-bold" onClick={handleLeave}>
-				Seite verlassen
+			<button
+				className="p-2 rounded-md max-w-md w-full bg-secondary-base/80 hover:bg-secondary-base/60 duration-100 text-primary-base"
+				onClick={handleLeave}
+				aria-label="Verlassen Sie die Website, wenn Sie unter 18 Jahre alt sind">
+				Ich bin unter 18 - verlassen
+			</button>
+		)
+	}
+
+	const EnterSiteButton = () => {
+		const handleEnter = () => {
+			setOpen(false)
+			localStorage.setItem("ageVerified", "true")
+		}
+
+		return (
+			<button
+				className="p-2 rounded-md max-w-md w-full bg-accent-base hover:bg-accent-base/80 duration-75 text-primary-base"
+				onClick={handleEnter}>
+				Ich bin 18 oder älter - enter
 			</button>
 		)
 	}
 
 	return open ? (
 		<div className="fixed flex flex-col z-50 w-full justify-center items-center h-full bg-black/90">
-			<div className=" text-xl bg-primary-base w-[300px] gap-2 h-28 flex flex-col">
-				<div className="w-full h-[50%] flex flex-row justify-center text-2xl text-gray font-semibold items-center">
-					Über 18?
+			<div className=" bg-primary-base w-[80%] gap-1 h-auto flex flex-col rounded-md">
+				<div className=" font-bold text-xl sm:text-2xl w-full text-center sm:p-6 p-2">
+					Wichtiger Hinweis: Inhalte nur für Erwachsene (18+)
 				</div>
-				<div className="w-full flex flex-row gap-8 justify-center items-center">
-					<button
-						onClick={() => {
-							setOpen(false)
-						}}
-						className="w-20 h-8 bg-accent-base text-white">
-						Ja
-					</button>
+				<div className="text-sm px-3 max-h-60  overflow-y-scroll tracking-tight flex flex-col gap-2">
+					<p>
+						Bevor Sie fortfahren, möchten wir Sie darauf aufmerksam machen, dass
+						diese Seite Inhalte enthält, die ausschließlich für Erwachsene
+						bestimmt sind. Mit dem Zugriff auf unsere Website bestätigen Sie,
+						dass Sie mindestens 18 Jahre alt sind und dass Sie mit der Ansicht
+						von Inhalten für Erwachsene einverstanden sind.
+					</p>
+					<p>
+						Unsere Inhalte sind vielfältig und sollen unterschiedliche
+						Interessen und Vorlieben ansprechen. Wir legen großen Wert darauf,
+						eine sichere und respektvolle Umgebung für alle Nutzer zu schaffen.
+						Wenn Sie Bedenken bezüglich der Inhalte haben oder Fragen zu unserer
+						Website haben, zögern Sie bitte nicht, uns zu kontaktieren.
+					</p>
+					<p>
+						Indem Sie fortfahren, übernehmen Sie die volle Verantwortung für
+						Ihre Handlungen und bestätigen, dass Sie die rechtlichen
+						Anforderungen für die Ansicht von Erwachseneninhalten in Ihrer
+						Region erfüllen. Wir übernehmen keine Haftung für etwaige
+						Konsequenzen, die sich aus Ihrer Entscheidung ergeben, auf unsere
+						Inhalte zuzugreifen.
+					</p>
+					<p>Wir schätzen Ihr Verständnis und Ihre Kooperation.</p>
+				</div>
+				<div className="w-full flex flex-col gap-2 p-4 justify-center items-center">
+					<EnterSiteButton />
 					<LeaveSiteButton />
 				</div>
 			</div>
